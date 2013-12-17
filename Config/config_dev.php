1ca2e3401a4a088fa930d147ace884ad4bd7e76a
Florian Quattrocchi <florianquattrocchi@gmail.com>
2013-12-17 22:46:28 +0100

    Authorizations

--- Config/config_dev.php
+++ Config/config_dev.php
@@ -7,7 +7,7 @@
 	define('APP_ID', '553248554770243');
 	define('APP_SECRET', '97a1373ba6f66485c40bec5d764faf20');
 	define('CALLBACK_URL', 'http://localhost/FB_Dashboard/');
-	define('AUTHORIZATIONS', 'read_stream, user_friends, friends_relationships, user_likes, friends_likes, friends_birthday');
+	define('AUTHORIZATIONS', 'read_stream, user_friends, friends_relationships, user_likes, friends_likes, friends_birthday, friends_hometown, friends_birthday, friends_location, friends_relationship_details, friends_work_history, friends_education_history');
 	
 
 
